0088030dec0f6ca2128695dd6ce52150ef515ae0
yueda1984 <61306261+yueda1984@users.noreply.github.com>
2024-04-23 18:14:40 +0900

    Folder structure update

--- src/DEF_Curve_Envelope_Converter.js
+++ src/DEF_Curve_Envelope_Converter.js
@@ -14,8 +14,8 @@
 	2) Locate to your user scripts folder (a hidden folder):
 	   https://docs.toonboom.com/help/harmony-17/premium/scripting/import-script.html	
 	   
-	3) Add all unzipped files (*.js, *.ui, and script-icons folder) directly to the folder above.	
-	4) Add DEF_Curve_Envelope_Converter to any toolbar.
+	3) There is a folder named "src" inside the zip file. Copy all its contents directly to the folder above.	
+	4) In Harmony, add DEF_Curve_Envelope_Converter function to any toolbar.
 	
 	
 	Direction:
